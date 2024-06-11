da6ba61b75d9fbcad3ba7550626771f40579a462
Keerthan Devera <Deverakeerthan3030@gmail.com>
2024-06-10 20:51:54 -0400

    Fixed the date

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -7,8 +7,14 @@ const Transaction = ({transaction}) => {
     const sign = transaction.amount < 0 ? '-' : '+' ;
     return (
         <li className={transaction.amount <0 ? 'minus' : 'plus'}>
-            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><div>{transaction.date} {transaction.time}</div><button onClick={()=> deleteTransaction(transaction.id)} className="delete-btn">x</button>
+            <div>{transaction.date}</div>
+            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+            <button onClick={()=> deleteTransaction(transaction.id)} className="delete-btn">x</button>
+            
+                
+            
         </li>
+       
   )
 }
 
